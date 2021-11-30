5068f863b274b6633a3a90859c51516d2d223b17
william <chenweibiao@kuaishou.com>
2021-11-30 12:30:36 +0800

    update mat4

--- math/mat4.h
+++ math/mat4.h
@@ -3,37 +3,45 @@
 //
 
 #pragma once
+
 #include "vec3.h"
-#include <iomanip>
+
 #include <cstdlib>
+#include <iomanip>
+
 template <typename T>
 class Mat4
 {
 public:
     Mat4() = default;
+
     ~Mat4() = default;
+
     Mat4(T a00, T a01, T a02, T a03, T a10, T a11, T a12, T a13,
          T a20, T a21, T a22, T a23, T a30, T a31, T a32, T a33)
     {
-        x[0][0] = a00;
-        x[0][1] = a01;
-        x[0][2] = a02;
-        x[0][3] = a03;
-        x[1][0] = a10;
-        x[1][1] = a11;
-        x[1][2] = a12;
-        x[1][3] = a13;
-        x[2][0] = a20;
-        x[2][1] = a21;
-        x[2][2] = a22;
-        x[2][3] = a23;
-        x[3][0] = a30;
-        x[3][1] = a31;
-        x[3][2] = a32;
-        x[3][3] = a33;
+        m[0][0] = a00;
+        m[0][1] = a01;
+        m[0][2] = a02;
+        m[0][3] = a03;
+        m[1][0] = a10;
+        m[1][1] = a11;
+        m[1][2] = a12;
+        m[1][3] = a13;
+        m[2][0] = a20;
+        m[2][1] = a21;
+        m[2][2] = a22;
+        m[2][3] = a23;
+        m[3][0] = a30;
+        m[3][1] = a31;
+        m[3][2] = a32;
+        m[3][3] = a33;
     }
-    const T* operator[](uint8_t i) const { return x[i]; }
-    T* operator[](uint8_t i) { return x[i]; }
+
+    const T* operator[](uint8_t i) const { return m[i]; }
+
+    T* operator[](uint8_t i) { return m[i]; }
+
     Mat4 operator*(const Mat4& v) const
     {
         Mat4 temp;
@@ -43,9 +51,9 @@ public:
 
     static void multiply(const Mat4& a, const Mat4& b, Mat4& dst)
     {
-        const T* __restrict ap = &a.x[0][0];
-        const T* __restrict bp = &b.x[0][0];
-        T* __restrict cp = &dst.x[0][0];
+        const T* __restrict ap = &a.m[0][0];
+        const T* __restrict bp = &b.m[0][0];
+        T* __restrict cp = &dst.m[0][0];
         T a0, a1, a2, a3;
         a0 = ap[0];
         a1 = ap[1];
@@ -83,42 +91,42 @@ public:
 
     Mat4 transposed() const
     {
-        return Matrix44(x[0][0],
-                        x[1][0],
-                        x[2][0],
-                        x[3][0],
-                        x[0][1],
-                        x[1][1],
-                        x[2][1],
-                        x[3][1],
-                        x[0][2],
-                        x[1][2],
-                        x[2][2],
-                        x[3][2],
-                        x[0][3],
-                        x[1][3],
-                        x[2][3],
-                        x[3][3]);
+        return Matrix44(m[0][0],
+                        m[1][0],
+                        m[2][0],
+                        m[3][0],
+                        m[0][1],
+                        m[1][1],
+                        m[2][1],
+                        m[3][1],
+                        m[0][2],
+                        m[1][2],
+                        m[2][2],
+                        m[3][2],
+                        m[0][3],
+                        m[1][3],
+                        m[2][3],
+                        m[3][3]);
     }
 
     Mat4& transposed()
     {
-        Mat4 tmp(x[0][0],
-                 x[1][0],
-                 x[2][0],
-                 x[3][0],
-                 x[0][1],
-                 x[1][1],
-                 x[2][1],
-                 x[3][1],
-                 x[0][2],
-                 x[1][2],
-                 x[2][2],
-                 x[3][2],
-                 x[0][3],
-                 x[1][3],
-                 x[2][3],
-                 x[3][3]);
+        Mat4 tmp(m[0][0],
+                 m[1][0],
+                 m[2][0],
+                 m[3][0],
+                 m[0][1],
+                 m[1][1],
+                 m[2][1],
+                 m[3][1],
+                 m[0][2],
+                 m[1][2],
+                 m[2][2],
+                 m[3][2],
+                 m[0][3],
+                 m[1][3],
+                 m[2][3],
+                 m[3][3]);
         *this = tmp;
         return *this;
     }
@@ -127,10 +135,10 @@ public:
     void multVecMatrix(const Vec3<S>& src, Vec3<S>& dst) const
     {
         S a, b, c, w;
-        a = src[0] * x[0][0] + src[1] * x[1][0] + src[2] * x[2][0] + x[3][0];
-        b = src[0] * x[0][1] + src[1] * x[1][1] + src[2] * x[2][1] + x[3][1];
-        c = src[0] * x[0][2] + src[1] * x[1][2] + src[2] * x[2][2] + x[3][2];
-        w = src[0] * x[0][3] + src[1] * x[1][3] + src[2] * x[2][3] + x[3][3];
+        a = src[0] * m[0][0] + src[1] * m[1][0] + src[2] * m[2][0] + m[3][0];
+        b = src[0] * m[0][1] + src[1] * m[1][1] + src[2] * m[2][1] + m[3][1];
+        c = src[0] * m[0][2] + src[1] * m[1][2] + src[2] * m[2][2] + m[3][2];
+        w = src[0] * m[0][3] + src[1] * m[1][3] + src[2] * m[2][3] + m[3][3];
 
         dst.x = a / w;
         dst.y = b / w;
@@ -142,111 +150,73 @@ public:
     {
         S a, b, c;
 
-        a = src[0] * x[0][0] + src[1] * x[1][0] + src[2] * x[2][0];
-        b = src[0] * x[0][1] + src[1] * x[1][1] + src[2] * x[2][1];
-        c = src[0] * x[0][2] + src[1] * x[1][2] + src[2] * x[2][2];
+        a = src[0] * m[0][0] + src[1] * m[1][0] + src[2] * m[2][0];
+        b = src[0] * m[0][1] + src[1] * m[1][1] + src[2] * m[2][1];
+        c = src[0] * m[0][2] + src[1] * m[1][2] + src[2] * m[2][2];
 
         dst.x = a;
         dst.y = b;
         dst.z = c;
     }
+
     /// Gauss-Jordan 消元求逆
-    Mat4 inverse() const
+    Mat4 inverse()
     {
-        int i, j, k;
-        Mat4 s;
-        Mat4 t(*this);
-
-        // Forward elimination
-        for (i = 0; i < 3; i++)
+        Mat4 mat;
+        for (unsigned column = 0; column < 4; ++column)
         {
-            int pivot = i;
-            /// 寻找主元
-            T pivotsize = t[i][i];
-
-            if (pivotsize < 0)
+            // Swap row in case our pivot point is not working
+            if (m[column][column] == 0)
             {
-                pivotsize = -pivotsize;
-            }
-
-            for (j = i + 1; j < 4; j++)
-            {
-                T tmp = t[j][i];
-
-                if (tmp < 0)
-                    tmp = -tmp;
-
-                if (tmp > pivotsize)
+                unsigned big = column;
+                for (unsigned row = 0; row < 4; ++row)
                 {
-                    pivot = j;
-                    pivotsize = tmp;
+                    if (fabs(m[row][column]) > fabs(m[big][column])) big = row;
+                }
+                // Print this is a singular matrix, return identity ?
+                if (big == column)
+                {
+                    fprintf(stderr, "Singular matrix\n");
+                }
+                // Swap rows
+                else
+                {
+                    for (unsigned j = 0; j < 4; ++j)
+                    {
+                        std::swap(m[column][j], m[big][j]);
+                        std::swap(mat.m[column][j], mat.m[big][j]);
+                    }
                 }
             }
-
-            if (pivotsize == 0)
+            // Set each row in the column to 0
+            for (unsigned row = 0; row < 4; ++row)
             {
-                // Cannot invert singular matrix
-                return Mat4();
-            }
-            /// 互换主元
-            if (pivot != i)
-            {
-                for (j = 0; j < 4; j++)
+                if (row != column)
                 {
-                    T tmp;
-
-                    tmp = t[i][j];
-                    t[i][j] = t[pivot][j];
-                    t[pivot][j] = tmp;
-
-                    tmp = s[i][j];
-                    s[i][j] = s[pivot][j];
-                    s[pivot][j] = tmp;
-                }
-            }
-
-            for (j = i + 1; j < 4; j++)
-            {
-                T f = t[j][i] / t[i][i];
-
-                for (k = 0; k < 4; k++)
-                {
-                    t[j][k] -= f * t[i][k];
-                    s[j][k] -= f * s[i][k];
+                    T coeff = m[row][column] / m[column][column];
+                    if (coeff != 0)
+                    {
+                        for (unsigned j = 0; j < 4; ++j)
+                        {
+                            m[row][j] -= coeff * m[column][j];
+                            mat.m[row][j] -= coeff * mat.m[column][j];
+                        }
+                        // Set the element to 0 for safety
+                        m[row][column] = 0;
+                    }
                 }
             }
         }
-
-        // Backward substitution
-        for (i = 3; i >= 0; --i)
+        // Set each element of the diagonal to 1
+        for (unsigned row = 0; row < 4; ++row)
         {
-            T f;
-
-            if ((f = t[i][i]) == 0)
+            for (unsigned column = 0; column < 4; ++column)
             {
-                // Cannot invert singular matrix
-                return Mat4();
-            }
-
-            for (j = 0; j < 4; j++)
-            {
-                t[i][j] /= f;
-                s[i][j] /= f;
-            }
-
-            for (j = 0; j < i; j++)
-            {
-                f = t[j][i];
-
-                for (k = 0; k < 4; k++)
-                {
-                    t[j][k] -= f * t[i][k];
-                    s[j][k] -= f * s[i][k];
-                }
+                mat.m[row][column] /= m[row][row];
             }
         }
-
-        return s;
+        *this = mat;
+        return *this;
     }
 
     const Mat4& invert()
@@ -255,21 +225,28 @@ public:
         return *this;
     }
 
-    friend std::ostream& operator<<(std::ostream& s, const Mat4& m)
+    friend std::ostream& operator<<(std::ostream& s, const Mat4& mat)
     {
         std::ios_base::fmtflags oldFlags = s.flags();
         int width = 12; // total with of the displayed number
         s.precision(5); // control the number of displayed decimals
         s.setf(std::ios_base::fixed);
-        s << "[" << std::setw(width) << m[0][0] << " " << std::setw(width) << m[0][1] << " " << std::setw(width) << m[0][2] << " " << std::setw(width) << m[0][3] << "\n"
-          << " " << std::setw(width) << m[1][0] << " " << std::setw(width) << m[1][1] << " " << std::setw(width) << m[1][2] << " " << std::setw(width) << m[1][3] << "\n"
-          << " " << std::setw(width) << m[2][0] << " " << std::setw(width) << m[2][1] << " " << std::setw(width) << m[2][2] << " " << std::setw(width) << m[2][3] << "\n"
-          << " " << std::setw(width) << m[3][0] << " " << std::setw(width) << m[3][1] << " " << std::setw(width) << m[3][2] << " " << std::setw(width) << m[3][3] << "]";
+        s << "[" << std::setw(width) << mat[0][0] << " " << std::setw(width) << mat[0][1] << " " << std::setw(width)
+          << mat[0][2] << " " << std::setw(width) << mat[0][3] << "\n"
+          << " " << std::setw(width) << mat[1][0] << " " << std::setw(width) << mat[1][1] << " " << std::setw(width)
+          << mat[1][2] << " " << std::setw(width) << mat[1][3] << "\n"
+          << " " << std::setw(width) << mat[2][0] << " " << std::setw(width) << mat[2][1] << " " << std::setw(width)
+          << mat[2][2] << " " << std::setw(width) << mat[2][3] << "\n"
+          << " " << std::setw(width) << mat[3][0] << " " << std::setw(width) << mat[3][1] << " " << std::setw(width)
+          << mat[3][2] << " " << std::setw(width) << mat[3][3] << "]";
         s.flags(oldFlags);
         return s;
     }
 
-    T x[4][4] = { { 1, 0, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 }, { 0, 0, 0, 1 } };
+    T m[4][4] = { { 1, 0, 0, 0 },
+                  { 0, 1, 0, 0 },
+                  { 0, 0, 1, 0 },
+                  { 0, 0, 0, 1 } };
 };
 
 using Mat4f = Mat4<float>;
