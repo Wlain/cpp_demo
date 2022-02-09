60812c04c5b790dd7ccccabe785fa74e5b14c92a
william <chenweibiao@kuaishou.com>
2022-02-09 21:54:42 +0800

    update res

--- qt_demo/main.cpp
+++ qt_demo/main.cpp
@@ -3,6 +3,8 @@
 int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
+    w.resize(1024, 768);
+    w.setWindowTitle("mini Draw");
     w.show();
     return a.exec();
 }
