2b6ba3e613a5fc953ce8a0a4d331fd1b73509083
william <chenweibiao@kuaishou.com>
2022-06-08 13:02:12 +0800

    bugfix compiler

--- cpp/inherits.cpp
+++ cpp/inherits.cpp
@@ -1,37 +1,37 @@
 //
 // Created by william on 2022/5/31.
 //
-
+#include <iostream>
 /*
 C++三大特性之：继承
  */
 namespace cpp
 {
 
-class A 
+class A
 {
 public:
-    int m_a{0};
+    int m_a{ 0 };
 };
 
-class B 
+class B
 {
 public:
-    int m_b{1};
+    int m_b{ 1 };
 };
 
-class C : public A, public B 
+class C : public A, public B
 {
 public:
-	int getA() { return A::m_a;}
-    int m_a{2};
+    int getA() { return A::m_a; }
+    int m_a{ 2 };
 };
 
-
-int inheritsTest() {
-    C *c = new C();
-    A *a = c;
-    B *b = c;
+int inheritsTest()
+{
+    C* c = new C();
+    A* a = c;
+    B* b = c;
     /// 注意：a的地址和c的地址是一样的，因为他们的第一个类成员变量是一样的，该地址都指向m_a变量的地址,但是由于类C中重定义了，
     /// 通过a->m_a 是0,通过c->m_a是2
 
@@ -45,11 +45,11 @@ int inheritsTest() {
     std::cout << "m_b(B)= " << c->m_b << std::endl;
     std::cout << "m_a(C) = " << c->m_a << std::endl;
     std::cout << "m_a(C->A) = " << c->getA() << std::endl;
-    int tempA = (int) (*(int *) (c));
+    int tempA = (int)(*(int*)(c));
     std::cout << "tempA = " << tempA << std::endl;
-    int tempB = (int) (*(int *) (c) + 1);
+    int tempB = (int)(*(int*)(c) + 1);
     std::cout << "tempB = " << tempB << std::endl;
-    int tempC = (int) (*(int *) (c) + 2);
+    int tempC = (int)(*(int*)(c) + 2);
     std::cout << "tempC = " << tempC << std::endl;
     /* output
     sizeof a = 4
