2eea84ffe47ff281d26a6edc764e0405d34be0e5
william <chenweibiao@kuaishou.com>
2023-12-11 21:02:52 +0800

    update

--- test/js/jsTest.cpp
+++ test/js/jsTest.cpp
@@ -20,40 +20,29 @@ void jsTest(int argc, char* argv[])
 
     // Create a new Isolate and make it the current one.
     v8::Isolate::CreateParams create_params;
-    create_params.array_buffer_allocator =
-        v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+    create_params.array_buffer_allocator = v8::ArrayBuffer::Allocator::NewDefaultAllocator();
     v8::Isolate* isolate = v8::Isolate::New(create_params);
     {
         v8::Isolate::Scope isolate_scope(isolate);
-
         // Create a stack-allocated handle scope.
         v8::HandleScope handle_scope(isolate);
-
         // Create a new context.
         v8::Local<v8::Context> context = v8::Context::New(isolate);
-
         // Enter the context for compiling and running the hello world script.
         v8::Context::Scope context_scope(context);
-
         {
             // Create a string containing the JavaScript source code.
-            v8::Local<v8::String> source =
-                v8::String::NewFromUtf8Literal(isolate, "'Hello' + ', World!'");
-
+            v8::Local<v8::String> source = v8::String::NewFromUtf8Literal(isolate, "'Hello' + ', World!'");
             // Compile the source code.
-            v8::Local<v8::Script> script =
-                v8::Script::Compile(context, source).ToLocalChecked();
-
+            v8::Local<v8::Script> script = v8::Script::Compile(context, source).ToLocalChecked();
             // Run the script to get the result.
             v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
-
             // Convert the result to an UTF8 string and print it.
             v8::String::Utf8Value utf8(isolate, result);
             printf("%s\n", *utf8);
         }
-
         {
-            const char csource[] = R"javascript(
+            constexpr const char jsSource[] = R"javascript(
                 let bytes = new Uint8Array([
                   0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00, 0x01, 0x07, 0x01,
                   0x60, 0x02, 0x7f, 0x7f, 0x01, 0x7f, 0x03, 0x02, 0x01, 0x00, 0x07,
@@ -66,22 +55,16 @@ void jsTest(int argc, char* argv[])
             )javascript";
 
             // Create a string containing the JavaScript source code.
-            v8::Local<v8::String> source =
-                v8::String::NewFromUtf8Literal(isolate, csource);
-
+            v8::Local<v8::String> source = v8::String::NewFromUtf8Literal(isolate, jsSource);
             // Compile the source code.
-            v8::Local<v8::Script> script =
-                v8::Script::Compile(context, source).ToLocalChecked();
-
+            v8::Local<v8::Script> script = v8::Script::Compile(context, source).ToLocalChecked();
             // Run the script to get the result.
             v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
-
             // Convert the result to a uint32 and print it.
             uint32_t number = result->Uint32Value(context).ToChecked();
             printf("3 + 4 = %u\n", number);
         }
     }
-
     // Dispose the isolate and tear down V8.
     isolate->Dispose();
     v8::V8::Dispose();
